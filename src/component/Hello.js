ab38c9b8d8449f0893becbe9822f7d982dbad637
leedong2897 <skyblue5030@naver.com>
2021-05-15 14:43:34 +0900

    props (2)

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,16 +1,14 @@
 import { useState } from 'react';
-export default function Hello(props) {
-    console.log(props)
+export default function Hello({ age }) {
     const [name, setName] = useState('Abel');
-    const [age, setAge] = useState(props.age);
+    const msg = age > 19 ? '성인입니다.' : '미성년자입니다.';
 
     return (
         <div>
-            <h2>{name}({age})</h2>
+            <h2>{name}({age}):({msg})</h2>
             <button
                 onClick={() => {
                     setName(name === 'Abel' ? 'Jane' : 'Abel');
-                    setAge(age + 1)
                 }}
             >
                 Change
