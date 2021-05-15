a640c652ad56fe11369eca74534a5776ee49c823
leedong2897 <skyblue5030@naver.com>
2021-05-15 13:36:33 +0900

    state (2)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import Hello from './component/Hello';
-import styles from './App.module.css'
 
 function App() {
   return (
     <div className="App">
+      <Hello />
+      <Hello />
       <Hello />
     </div>
   );
 }
 
 export default App;
+
+/*
+  동일한 컴포넌트라도 state 는 각각 관리되므로
+  컴포넌트간의 영향을 받지 않음
+*/
--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,17 +1,13 @@
+import { useState } from 'react';
 export default function Hello() {
-    let name = 'Abel';
+    const [name, setName] = useState('Abel');
 
     function changeName() {
-        name = name === 'Abel' ? 'Jane' : 'Abel';
-        console.log(name)
-        //name 은 변경되었지만, DOM 이 업데이트가 안됨
-        //위 'Abel' 은 상태(state) 가 아닌 단순한 문자열(값)임
-        //위 값을 상태값으로 바꾸고, setState 함수를 사용하여
-        //상태값을 변경해야함 
+        const newName = name === 'Abel' ? 'Jane' : 'Abel';
+        setName(newName)
     }
     return (
         <div>
-            <h1>state</h1>
             <h2>{name}</h2>
             <button onClick={changeName}>Change</button>
         </div>);
