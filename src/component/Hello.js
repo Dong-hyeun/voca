b431d27b726bd37686ced4cbd19728d41a093eae
leedong2897 <skyblue5030@naver.com>
2021-05-15 14:48:29 +0900

    한 컴포넌트가 가지고 있는 state를 props 로 넘기기

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import UserName from './UserName';
 export default function Hello({ age }) {
     const [name, setName] = useState('Abel');
     const msg = age > 19 ? '성인입니다.' : '미성년자입니다.';
@@ -6,6 +7,7 @@ export default function Hello({ age }) {
     return (
         <div>
             <h2>{name}({age}):({msg})</h2>
+            <UserName name={name} />
             <button
                 onClick={() => {
                     setName(name === 'Abel' ? 'Jane' : 'Abel');
---
+++ src/component/UserName.js
@@ -0,0 +1,4 @@
+// 한 컴포넌트가 가지고 있는 state를 props 로 넘기기
+export default function UserName({ name }) {
+    return <p>Hello, {name}</p>
+}
