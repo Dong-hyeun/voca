c98c341b6ff0fcef51d1c6e22ed0f5c96bf0532a
leedong2897 <skyblue5030@naver.com>
2021-05-15 13:06:03 +0900

    react event(2)

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -3,16 +3,28 @@ export default function Hello() {
     function showName() {
         console.log('Abel');
     }
-
+    function showAge(age) {
+        console.log(age)
+    }
+    function showText(txt) {
+        console.log(txt)
+    }
     return (
         <div>
             <h1>Hello</h1>
             <button onClick={showName}>Show name</button>
             <button onClick={
                 () => {
-                    console.log(30)
-                }
-            }>Show age</button>
+                    showAge(10)
+                }}
+            >
+                Show age
+            </button>
+            <input type="text" onChange={e => {
+                const txt = e.target.value;
+                showText(txt)
+            }}
+            />
 
         </div>);
 }
