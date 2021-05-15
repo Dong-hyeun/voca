970eca3514796e41bb79a8cf847a16790f1cdbd9
leedong2897 <skyblue5030@naver.com>
2021-05-15 12:37:04 +0900

    react styling

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,10 +1,19 @@
-import World from "./World";
-
+// import './Hello.css';
+import styles from './Hello.module.css';
 export default function Hello() {
+
     return (
         <div>
-            <h2>Hello</h2>
-            <World />
-            <World />
+            <h2 style={
+                {
+                    color: '#f00',
+                    borderRight: '5px solid #000',
+                    marginBottom: '50px',
+                    opacity: 1,
+                }
+            }>
+                Hello
+            </h2>
+            <div className={styles.box} >Hello</div>
         </div>);
 }
